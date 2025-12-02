16296ed07c9a62576876cdeb6215b5c9bb8019ea
riyalagare8-rgb <riyalagare8@gmail.com>
2025-12-02 12:29:32 +0530

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -1,11 +1,11 @@
 
 
-def vehicle_registration(number, name, vehicle_type, manifacture):
+def vehicle_registration(number, name, vehicle_type, manufacture):
     result = (
         f"Vehicle number:{number}\n"
         f"vehicle owner name:{name}\n"
         f"vehicle type:{vehicle_type}\n"
-        f"year of manufacture:{manifacture}"
+        f"year of manufacture:{manufacture}"
     )
     return result
 
@@ -13,5 +13,5 @@ if _name_ == "_main_":
     number = "KA31N2881"
     name = "riya"
     vehicle_type = "car"
-    manifacture = 2023
-    print(vehicle_registration(number, name, vehicle_type, manifacture))
+    manufacture = 2023
+    print(vehicle_registration(number, name, vehicle_type,manufacture))
