8e53996c525068956267257a98204252dbb6ed53
riyalagare8-rgb <riyalagare8@gmail.com>
2025-12-02 12:11:50 +0530

    Update test_vehicle.py

--- test_vehicle.py
+++ test_vehicle.py
@@ -2,9 +2,9 @@ from vehicle import vehicle_registration
 
 def test_vehicle_registration():
     expected_output=(
-      Vehicle number="1234"
-   Vehicle owner name="riya"
+    Vehicle number="KA31N2881"
+    Vehicle owner name="riya"
     Vehicle type="car"
     Year of manifacture=2023
     )
-    assert vehicle_registration("1234","riya","car",2023)==expected_output
+    assert vehicle_registration("KA31N2881","riya","car",2023)==expected_output
