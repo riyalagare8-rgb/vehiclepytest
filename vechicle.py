1dd890ae948edb4f5eb5ff4e72ae0c1edf75f0fd
riyalagare8-rgb <riyalagare8@gmail.com>
2025-12-02 11:52:57 +0530

    Create vechicle.py

---
+++ vechicle.py
@@ -0,0 +1,14 @@
+def vehicleregistration_details(vehicleno,ownername,vechicetype,yearofmanufacture):
+    result=(
+        f"vehicle No:{no}\n"
+        f"owner name:{name}\n"
+        f"vehicle type:{type}\n"
+        f"year of manufacture:{manufacture}"
+    )
+    return result
+if __name__=="__main__":
+    vechicle no="1234"
+    ownwe name="riya"
+    vechicle type="car"
+    year of manufacture=2023
+    print(vehicleregistration_details(no,name,type,manufacture))
